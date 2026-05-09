cb9fcd7248ee168c9f59b21abf354bbb80eeae0e
UmangSaluja10 <umangsaluja99@gmail.com>
2026-05-09 15:43:24 +0530

    Add Railway deployment config + auto model download

--- flutter_app/android/build.gradle.kts
+++ flutter_app/android/build.gradle.kts
@@ -1,3 +1,6 @@
+plugins {
+    id("com.google.gms.google-services") version "4.3.15" apply false
+}
 allprojects {
     repositories {
         google()
@@ -21,7 +24,4 @@ subprojects {
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
-}
-dependencies {
-    classpath 'com.google.gms:google-services:4.4.2'
 }
